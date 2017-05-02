9b4f05acc5ccfc8d341cfdb42f8263f26b78bac0
Allen Hsu <allenhsu.taiwan@gmail.com>
2017-05-02 23:17:44 +0800

    finish project1

--- Project1-TapCounter/TapCounter/TapCounter/ViewController.swift
+++ Project1-TapCounter/TapCounter/TapCounter/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
-
+import RxSwift
+import RxCocoa
 class ViewController: UIViewController {
 
+	
+	@IBOutlet weak var counterNumber: UILabel!
+	@IBOutlet weak var tapButton: UIButton!
+	
+	private let disposeBag = DisposeBag()
+	private var count = 0
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		self.title = "Counter"
+		self.navigationController?.navigationBar.barTintColor = UIColor.white
+		self.navigationController?.navigationBar.clipsToBounds = true
+		
+		let leftButton = UIBarButtonItem(title: "Reset", style: .plain, target: nil, action: nil)
+		self.navigationItem.leftBarButtonItem = leftButton
+		
+		leftButton.rx.tap.subscribe(onNext: { [unowned self] _ in
+			self.count = 0
+			self.counterNumber.text = "\(self.count)"
+		}).addDisposableTo(disposeBag)
+		
 		// Do any additional setup after loading the view, typically from a nib.
+		self.tapButton.rx.tap.subscribe(onNext: { [unowned self] _ in
+			self.count += 1
+			self.counterNumber.text = "\(self.count)"
+		}).addDisposableTo(disposeBag)
 	}
 
 	override func didReceiveMemoryWarning() {
